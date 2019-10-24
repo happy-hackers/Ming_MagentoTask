60c77469c09950551109f519693bf77ca2cb92ea
DarylzzZ <boao.li@happyhackers.com.au>
2019-10-25 00:18:54 +1100

    1. overwrite customer save contorller 2. add customize events

--- app/code/Custom/Email/Controller/Adminhtml/Index/Save.php
+++ app/code/Custom/Email/Controller/Adminhtml/Index/Save.php
@@ -36,15 +36,140 @@ class Save extends Action implements HttpPostActionInterface
         $this->eventManager = $eventManager;
         return parent::__construct($context);
     }
-
     public function execute()
     {
+        $returnToEdit = false;
+        $originalRequestData = $this->getRequest()->getPostValue();
 
         $customerId = $this->getCurrentCustomerId();
 
-        if($customerId !== null){
-            $this->eventManager->dispatch('customer_change_info', ['customer' => $customer, 'request' => $this->getRequest()]);
+        if ($originalRequestData) {
+            try {
+                // optional fields might be set in request for future processing by observers in other modules
+                $customerData = $this->_extractCustomerData();
+                $addressesData = $this->_extractCustomerAddressData($customerData);
+
+                if ($customerId) {
+                    $currentCustomer = $this->_customerRepository->getById($customerId);
+                    $customerData = array_merge(
+                        $this->customerMapper->toFlatArray($currentCustomer),
+                        $customerData
+                    );
+                    $customerData['id'] = $customerId;
+                }
+
+                /** @var CustomerInterface $customer */
+                $customer = $this->customerDataFactory->create();
+                $this->dataObjectHelper->populateWithArray(
+                    $customer,
+                    $customerData,
+                    \Magento\Customer\Api\Data\CustomerInterface::class
+                );
+                $addresses = [];
+                foreach ($addressesData as $addressData) {
+                    $region = isset($addressData['region']) ? $addressData['region'] : null;
+                    $regionId = isset($addressData['region_id']) ? $addressData['region_id'] : null;
+                    $addressData['region'] = [
+                        'region' => $region,
+                        'region_id' => $regionId,
+                    ];
+                    $addressDataObject = $this->addressDataFactory->create();
+                    $this->dataObjectHelper->populateWithArray(
+                        $addressDataObject,
+                        $addressData,
+                        \Magento\Customer\Api\Data\AddressInterface::class
+                    );
+                    $addresses[] = $addressDataObject;
+                }
+
+                $this->_eventManager->dispatch(
+                    'adminhtml_customer_prepare_save',
+                    ['customer' => $customer, 'request' => $this->getRequest()]
+                );
+                $customer->setAddresses($addresses);
+                if (isset($customerData['sendemail_store_id'])) {
+                    $customer->setStoreId($customerData['sendemail_store_id']);
+                }
+
+                // Save customer
+                if ($customerId) {
+                    // save customer
+                    $this->eventManager->dispatch('customer_change_info',['customer' => $customer, 'request' => $this->getRequest()]);
+                    $this->_customerRepository->save($customer);
+
+                    $this->getEmailNotification()->credentialsChanged($customer, $currentCustomer->getEmail());
+                } else {
+                    // new customer
+                    $customer = $this->customerAccountManagement->createAccount($customer);
+                    $customerId = $customer->getId();
+                }
+
+                $isSubscribed = null;
+                if ($this->_authorization->isAllowed(null)) {
+                    $isSubscribed = $this->getRequest()->getPost('subscription');
+                }
+                if ($isSubscribed !== null) {
+                    if ($isSubscribed !== '0') {
+                        $this->_subscriberFactory->create()->subscribeCustomerById($customerId);
+                    } else {
+                        $this->_subscriberFactory->create()->unsubscribeCustomerById($customerId);
+                    }
+                }
+
+                // After save
+                $this->_eventManager->dispatch(
+                    'adminhtml_customer_save_after',
+                    ['customer' => $customer, 'request' => $this->getRequest()]
+                );
+                $this->_getSession()->unsCustomerFormData();
+                // Done Saving customer, finish save action
+                $this->_coreRegistry->register(RegistryConstants::CURRENT_CUSTOMER_ID, $customerId);
+                $this->messageManager->addSuccess(__('You saved the customer.'));
+                $returnToEdit = (bool)$this->getRequest()->getParam('back', false);
+            } catch (\Magento\Framework\Validator\Exception $exception) {
+                $messages = $exception->getMessages();
+                if (empty($messages)) {
+                    $messages = $exception->getMessage();
+                }
+                $this->_addSessionErrorMessages($messages);
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            } catch (\Magento\Framework\Exception\AbstractAggregateException $exception) {
+                $errors = $exception->getErrors();
+                $messages = [];
+                foreach ($errors as $error) {
+                    $messages[] = $error->getMessage();
+                }
+                $this->_addSessionErrorMessages($messages);
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            } catch (LocalizedException $exception) {
+                $this->_addSessionErrorMessages($exception->getMessage());
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            } catch (\Exception $exception) {
+                $this->messageManager->addException($exception, __('Something went wrong while saving the customer.'));
+                $this->_getSession()->setCustomerFormData($originalRequestData);
+                $returnToEdit = true;
+            }
         }
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($returnToEdit) {
+            if ($customerId) {
+                $resultRedirect->setPath(
+                    'customer/*/edit',
+                    ['id' => $customerId, '_current' => true]
+                );
+            } else {
+                $resultRedirect->setPath(
+                    'customer/*/new',
+                    ['_current' => true]
+                );
+            }
+        } else {
+            $resultRedirect->setPath('customer/index');
+        }
+        return $resultRedirect;
     }
     /**
      * Retrieve current customer ID
